91d5744241c6f10e0148371a7970b9d24bc25313
Ian Sabey <isabey@sofi.org>
2018-02-27 09:11:15 -0700

    google login button showing on '/' linking to /auth/google

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import triage from './triage.svg';
+import { GoogleLoginButton } from 'react-social-login-buttons';
+import Center from 'react-center';
 import './App.css';
 
 class App extends Component {
@@ -10,7 +12,11 @@ class App extends Component {
           <img src={triage} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to the Release Triage Toolkit!</h1>
         </header>
-        <a href="/auth/google">Sign In</a>
+        <a href="/auth/google">
+          <Center>
+            <GoogleLoginButton href="/auth/google" />
+          </Center>
+        </a>
       </div>
     );
   }
