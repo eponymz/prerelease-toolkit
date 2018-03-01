38a3acb92129b9973157050192e4e4aafda36ce8
Ian Sabey <isabey@sofi.org>
2018-03-01 10:45:27 -0700

    client/server integration ... finally

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
-// import { BrowserRouter, Route } from 'react-router-dom';
-// import triage from '../triage.svg';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 import '../css/Header.css';
 
 class Dashboard extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <Redirect to="/" />;
+      default:
+        return (
+          <div className="navbar-title">
+            <h1>HELLO</h1>
+          </div>
+        );
+    }
+  }
+
   render() {
-    return (
-      <div className="navbar-title">
-        <h1>HELLO</h1>
-      </div>
-    );
+    return <div>{this.renderContent()}</div>;
   }
 }
 
-export default Dashboard;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Dashboard);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import triage from '../triage.svg';
 import logout from '../logout.svg';
@@ -6,26 +7,39 @@ import '../css/Header.css';
 import '../css/GoogleBtn.css';
 
 class Header extends Component {
-  render() {
-    console.log(this.props);
-    return (
-      <div>
-        <header className="Header navbar-header">
-          <div className="image-header">
-            <img src={triage} className="Header-logo" alt="logo" />
-            <a className="no-underline navbar-title" href="/triage/dashboard">
-              T R I A G E
-            </a>
-            <a className="right component-title" href="/">
-              <div className="logout-icon">
-                <img src={logout} className="logout-icon" alt="logo" />
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <Redirect to="/" />;
+      default:
+        return (
+          <div>
+            <header className="Header navbar-header">
+              <div className="image-header">
+                <img src={triage} className="Header-logo" alt="logo" />
+                <a
+                  className="no-underline navbar-title"
+                  href="/triage/dashboard"
+                >
+                  T R I A G E
+                </a>
+                <a className="right component-title" href="/api/logout">
+                  <div className="logout-icon">
+                    <img src={logout} className="logout-icon" alt="logo" />
+                  </div>
+                  L O G O U T
+                </a>
               </div>
-              L O G O U T
-            </a>
+            </header>
           </div>
-        </header>
-      </div>
-    );
+        );
+    }
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 
