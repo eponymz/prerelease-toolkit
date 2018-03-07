c709b4d7fa02d4808e41bcad5f1cb6091ee4eb91
Ian Sabey <isabey@sofi.org>
2018-03-07 09:47:56 -0700

    removed rrr legend

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -28,39 +28,6 @@ class Dashboard extends Component {
                     frameborder="2px"
                   />
                 </Center>
-                <div className="relic-left">
-                  <p style={{ textAlign: 'center' }}>Risk Rating Legend</p>
-                  <Center>
-                    <table
-                      style={{
-                        width: '100%',
-                        textAlign: 'center',
-                        borderSpacing: '0px'
-                      }}
-                    >
-                      <tr>
-                        <th>
-                          <h3>Label</h3>
-                        </th>
-                        <th>
-                          <h3>Rating</h3>
-                        </th>
-                      </tr>
-                      <tr>
-                        <td>rrr1</td>
-                        <td>RED</td>
-                      </tr>
-                      <tr>
-                        <td>rrr2</td>
-                        <td>YELLOW</td>
-                      </tr>
-                      <tr>
-                        <td>rrr3</td>
-                        <td>GREEN</td>
-                      </tr>
-                    </table>
-                  </Center>
-                </div>
               </div>
             </Center>
           </div>
