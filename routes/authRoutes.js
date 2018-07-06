326464f30d2a45811072926ade62d01eacdbb6cb
Ian Sabey <isabey@sofi.org>
2018-07-06 16:11:01 -0600

    makin things do things and using tests that dont test my actual shit. #winning

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -26,9 +26,8 @@ module.exports = app => {
 
   app.get('/api/current_user', (req, res) => {
     // res.send(req.session);
-    //res.send(req.user);
-    res.sendStatus(200);
-    res.send.JSON(req.user);
+    res.send(req.user);
+    //res.status(200).JSON(req.user);
   });
 
   // app.post('/z/no-auth', (req, res, next) => {
